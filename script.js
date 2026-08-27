fb677507ece767b56681596a5b694f001900d085
its-lee-yo <leo04ru@icloud.com>
2026-08-27 07:06:28 +0200

    Update and rename script.js to stle.css

--- script.js
+++
@@ -1,8 +0,0 @@
-function unterschreiben() {
-    let name = prompt("Gib deinen Namen ein:");
-    if (name && name.trim() !== "") {
-        document.getElementById("unterschrift").innerHTML = name + " hat den Ehrenkodex unterschrieben! Danke!";
-    } else if (name !== null) {
-        alert("Bitte gib deinen Namen ein.");
-    }
-}
